6ecd9d34c0becf1cee31e52fa39e435ed1f3568c
raphaelbernhart <raphael.bernhart@clubdrei.com>
2023-07-03 22:39:00 +0200

    🎨 Improve hetzner cloud integration

--- src/types/hcloud.js.d.ts
+++ src/types/hcloud.js.d.ts
@@ -1,74 +1,87 @@
-declare module 'hcloud-js' {
+declare module "hcloud-js" {
     class Client {
-        new (token: string): Promise<HetznerClient>
-        constructor(token: string)
-        actions: HetznerActionsEndpoint
-        servers: ServersEndpoint
-        floatingIPs: FloatingIps
-        sshKeys: SshKeys
-        serverTypes: ServerTypes
-        locations: Locations
-        datacenters: Datacenters
-        images: Images
-        isos: Isos
+        new(token: string): Promise<HetznerClient>;
+        constructor(token: string);
+        actions: HetznerActionsEndpoint;
+        servers: ServersEndpoint;
+        floatingIPs: FloatingIps;
+        sshKeys: SshKeys;
+        serverTypes: ServerTypes;
+        locations: Locations;
+        datacenters: Datacenters;
+        images: Images;
+        isos: Isos;
     }
 }
 
 interface HetznerClient {
-    actions: HetznerActionsEndpoint,
-    servers: ServersEndpoint,
-    floatingIPs: FloatingIps,
-    sshKeys: SshKeys,
-    serverTypes: ServerTypes,
-    locations: Locations,
-    datacenters: Datacenters,
-    images: Images,
-    isos: Isos
+    actions: HetznerActionsEndpoint;
+    servers: ServersEndpoint;
+    floatingIPs: FloatingIps;
+    sshKeys: SshKeys;
+    serverTypes: ServerTypes;
+    locations: Locations;
+    datacenters: Datacenters;
+    images: Images;
+    isos: Isos;
+    hCloudToken: {
+        token: string;
+        name: string;
+    };
 }
 
 interface HetznerActionsEndpoint {
-    list: { (params: Record<string, any>): Promise<Array<HetznerAction>> },
-    get: { (id: number): Promise<HetznerAction> }
+    list: { (params: Record<string, any>): Promise<Array<HetznerAction>> };
+    get: { (id: number): Promise<HetznerAction> };
 }
 
 interface HetznerAction {
-    command: string,
-    error: HetznerError,
-    finished: string,
-    id: number,
-    progress: number,
-    resources: Array<Record<string, { id: number, type: string }>>,
-    started: string,
-    status: string
+    command: string;
+    error: HetznerError;
+    finished: string;
+    id: number;
+    progress: number;
+    resources: Array<Record<string, { id: number; type: string }>>;
+    started: string;
+    status: string;
 }
 
 interface ServersEndpoint {
-    list: { (params?: Record<string, any>): Promise<ServerList> }
+    list: { (params?: Record<string, any>): Promise<ServerList> };
 }
 
 interface Server {
-    backup_window: string,
-    created: string,
-    datacenter: Record<string, any>,
-    id: number,
-    image: Record<string, any>,
-    included_traffic: number,
-    ingoing_traffic: number,
-    iso: Record<string, any>,
-    labels: Record<string, any>,
-    load_balancers: Array<number>,
-    locked: boolean,
-    name: string,
-    outgoing_traffic: number,
-    placement_group: object,
-    primary_disk_size: number,
-    private_net: Array<Record<string, any>>,
-    protection: Record<string, any>,
-    public_net: Record<string, any>,
-    rescue_enabled: boolean,
-    server_type: Record<string, any>,
-    status: 'initializing'|'starting'|'running'|'stopping'|'off'|'deleting'|'rebuilding'|'migrating'|'unknown',
-    volumes: Array<number>
+    backup_window: string;
+    created: string;
+    datacenter: Record<string, any>;
+    id: number;
+    image: Record<string, any>;
+    included_traffic: number;
+    ingoing_traffic: number;
+    iso: Record<string, any>;
+    labels: Record<string, any>;
+    load_balancers: Array<number>;
+    locked: boolean;
+    name: string;
+    outgoing_traffic: number;
+    placement_group: object;
+    primary_disk_size: number;
+    private_net: Array<Record<string, any>>;
+    protection: Record<string, any>;
+    public_net: Record<string, any>;
+    rescue_enabled: boolean;
+    server_type: Record<string, any>;
+    status:
+        | "initializing"
+        | "starting"
+        | "running"
+        | "stopping"
+        | "off"
+        | "deleting"
+        | "rebuilding"
+        | "migrating"
+        | "unknown";
+    volumes: Array<number>;
 }
 
 interface FloatingIps {}
@@ -76,9 +89,9 @@ interface FloatingIps {}
 interface SshKeys {}
 
 interface ServerList {
-    endpoint: any,
-    params: Record<string, any>,
-    servers: Array<Server>
+    endpoint: any;
+    params: Record<string, any>;
+    servers: Array<Server>;
 }
 
 interface ServerTypes {}
@@ -87,12 +100,11 @@ interface Locations {}
 
 interface Datacenters {}
 
-
 interface Images {}
 
 interface Isos {}
 
 interface HetznerError {
-    code: string,
-    message: string
+    code: string;
+    message: string;
 }
--- src/vendor/hetznerCloud.ts
+++ src/vendor/hetznerCloud.ts
@@ -1,39 +1,64 @@
-import HetznerCloud from "hcloud-js";
+import hcloud from "hcloud-js";
 import consola from "consola";
 
-const ClientInit = async (token: string): Promise<HetznerClient> => {
-    const client: HetznerClient = (await new HetznerCloud.Client(
-        "yMwNq6qViw7oJs1Bm1UkBRHO8KFHggjSDKx0N64K8VW0SvkNrESCDOjs3qoDQ0Qw"
-    )) as HetznerClient;
+export default class HetznerCloud {
+    static hCloudClients: Array<HetznerClient>;
 
-    // Test client
-    try {
-        await client.servers.list();
-    } catch (err: any) {
-        consola.error("[Hetzner API] " + err.message);
-        process.exit(1);
+    constructor(clients: Array<HetznerClient>) {
+        HetznerCloud.hCloudClients = clients;
+
+        // Test HCloud Client
+        HetznerCloud.hCloudClients.forEach((client: HetznerClient) => {
+            client.servers.list().catch((err: any) => {
+                if (!err) return;
+
+                if (err.code === "unauthorized") {
+                    consola.error(
+                        `[Hetzner API] Could not authenticate token '${client.hCloudToken.name}'`
+                    );
+                } else {
+                    consola.error(`[Hetzner API] ${err.message}`);
+                }
+                process.exit(1);
+            });
+        });
     }
 
-    return client;
-};
+    static async initHCloud() {
+        let i = 1;
+        let currentHToken: string = process.env[`HETZNER_TOKEN_${i}`] || "";
+        const ClientsArray: Array<HetznerClient> = [];
 
-export default async (): Promise<Array<HetznerClient>> => {
-    let i = 1;
-    let currentHToken: string = process.env[`HETZNER_TOKEN_${i}`] || "";
-    const ClientsArray: Array<HetznerClient> = [];
+        while (currentHToken) {
+            i++;
 
-    while (currentHToken) {
-        i++;
+            ClientsArray.push({
+                ...(await this.initSingleHCloudClient(currentHToken)),
+                hCloudToken: {
+                    token: currentHToken,
+                    name: `HETZNER_TOKEN_${i - 1}`,
+                },
+            });
 
-        ClientsArray.push(await ClientInit(currentHToken));
+            // Update current Token
+            currentHToken = process.env[`HETZNER_TOKEN_${i}`] || "";
+        }
 
-        // Update current Token
-        currentHToken = process.env[`HETZNER_TOKEN_${i}`] || "";
+        await Promise.all(ClientsArray);
+
+        globalThis.$hcloud = ClientsArray;
+
+        return new HetznerCloud(ClientsArray);
     }
 
-    Promise.all(ClientsArray);
-
-    globalThis.$hcloud = ClientsArray;
-
-    return ClientsArray;
-};
+    static async initSingleHCloudClient(token: string) {
+        try {
+            // "yMwNq6qViw7oJs1Bm1UkBRHO8KFHggjSDKx0N64K8VW0SvkNrESCDOjs3qoDQ0Qw"
+            const client = await new hcloud.Client(token);
+            return client;
+        } catch (err: any) {
+            consola.error(`[Hetzner API] ${err.message}`);
+            process.exit(1);
+        }
+    }
+}
