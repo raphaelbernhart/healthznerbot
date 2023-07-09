d1829af90a7c1aeab34f573ff54254fbd36fc1a8
raphaelbernhart <raphael.bernhart@clubdrei.com>
2023-07-09 14:43:23 +0200

    🚑 Hotfix de lang net reference

--- src/lang/de.ts
+++ src/lang/de.ts
@@ -1,7 +1,7 @@
 const lang = {
     status: {
         failed: (server: Record<string, any>) => {
-            return `❌ Server **${server.name}**(${server.public_net.ipv4.ip}) ist momentan offline.`;
+            return `❌ Server **${server.name}**(${server.publicNet.ipv4.ip}) ist momentan offline.`;
         },
         success: "🟢 Alle Server sind online!",
     },
